efc3bd71efd6cec17e589ecde1ace749529fb166
alirdurak <durakaliriza17@gmail.com>
2023-11-07 16:18:48 +0300

    First Project Created

---
+++ Fİrst-Project-07.11.2023/src/App.js
@@ -0,0 +1,98 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <img
+          src="https://th.bing.com/th/id/OIP.4dcJ_AHTJ81dikKbJ_xBtgHaGw?pid=ImgDet&rs=1"
+          alt=""
+        />
+        <div className="nav">
+          <span>Anasayfa</span>
+          <span>Hakkında</span>
+          <span>İletişim</span>
+          <span>Ürünlerimiz</span>
+        </div>
+      </div>
+      <div className="main">
+        <div className="category">
+          <span>Bilgisayarlar</span>
+          <span>Telefonlar</span>
+          <span>Saatler</span>
+          <span>Aksesuarlar</span>
+        </div>
+        <div className="products">
+          <div className="card">
+            <img
+              src="https://th.bing.com/th/id/OIP.4dcJ_AHTJ81dikKbJ_xBtgHaGw?pid=ImgDet&rs=1"
+              alt=""
+            />
+            <h2> Urun</h2>
+            <p>
+              Lorem ipsum dolor sit amet.<span>12.99TL</span>
+            </p>
+            <button>Sepete ekle</button>
+          </div>
+          <div className="card">
+            <img
+              src="https://th.bing.com/th/id/OIP.4dcJ_AHTJ81dikKbJ_xBtgHaGw?pid=ImgDet&rs=1"
+              alt=""
+            />
+            <h2> Urun</h2>
+            <p>
+              Lorem ipsum dolor sit amet.<span>12.99TL</span>
+            </p>
+            <button>Sepete ekle</button>
+          </div>
+          <div className="card">
+            <img
+              src="https://th.bing.com/th/id/OIP.4dcJ_AHTJ81dikKbJ_xBtgHaGw?pid=ImgDet&rs=1"
+              alt=""
+            />
+            <h2> Urun</h2>
+            <p>
+              Lorem ipsum dolor sit amet.<span>12.99TL</span>
+            </p>
+            <button>Sepete ekle</button>
+          </div>
+          <div className="card">
+            <img
+              src="https://th.bing.com/th/id/OIP.4dcJ_AHTJ81dikKbJ_xBtgHaGw?pid=ImgDet&rs=1"
+              alt=""
+            />
+            <h2> Urun</h2>
+            <p>
+              Lorem ipsum dolor sit amet.<span>12.99TL</span>
+            </p>
+            <button>Sepete ekle</button>
+          </div>
+          <div className="card">
+            <img
+              src="https://th.bing.com/th/id/OIP.4dcJ_AHTJ81dikKbJ_xBtgHaGw?pid=ImgDet&rs=1"
+              alt=""
+            />
+            <h2> Urun</h2>
+            <p>
+              Lorem ipsum dolor sit amet.<span>12.99TL</span>
+            </p>
+            <button>Sepete ekle</button>
+          </div>
+          <div className="card">
+            <img
+              src="https://th.bing.com/th/id/OIP.4dcJ_AHTJ81dikKbJ_xBtgHaGw?pid=ImgDet&rs=1 "
+              alt=""
+            />
+            <h2> Urun</h2>
+            <p>
+              Lorem ipsum dolor sit amet.<span>12.99TL</span>
+            </p>
+            <button>Sepete ekle</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
