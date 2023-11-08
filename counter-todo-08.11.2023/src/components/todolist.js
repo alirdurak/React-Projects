3efffb7841f4c355579f1d0c39706605fde1ee26
alirdurak <durakaliriza17@gmail.com>
2023-11-08 10:30:44 +0300

    counter and todo created

---
+++ counter-todo-08.11.2023/src/components/todolist.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+export function Todolist() {
+  const [list, setList] = useState([
+    "hjafhajkfh",
+    "ahdkjsahdkjashdjkas",
+    "klafkajsdklajskdjaksd",
+  ]);
+  const [todo, setTodo] = useState(" ");
+
+  const addTodo = () => {
+    setList([...list, todo]);
+    setTodo(" ");
+  };
+  return (
+    <div>
+      {list.map((i, j) => {
+        return (
+          <span key={j}>
+            {i}
+            <br />
+          </span>
+        );
+      })}
+      <div>
+        <input
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+          type="text"
+        />
+        <button onClick={addTodo}>Ekle</button>
+      </div>
+    </div>
+  );
+}
