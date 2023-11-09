e7fbc036c2a86f126303a3f45faa2a5735c355f8
alirdurak <durakaliriza17@gmail.com>
2023-11-09 08:25:32 +0300

    notes app creating with bootstrapp

---
+++ notes-08.11.2023/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Note from "./components/note";
+
+function App() {
+  return (
+    <div className="App">
+      <Note></Note>
+    </div>
+  );
+}
+
+export default App;
